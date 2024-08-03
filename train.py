7de556757874495ad29be589aea128631689f3ce
Ethan <ewer@wisc.edu>
2024-08-03 08:47:39 -0500

    wandb name update

--- train.py
+++ train.py
@@ -222,7 +222,7 @@ if __name__ == "__main__":
             "betas": BETAS,
             "seed": SEED,
         },
-        name=TASK,
+        name=CHECKPOINT_NAME[:-3],
         resume=RESUME,
     )
 
