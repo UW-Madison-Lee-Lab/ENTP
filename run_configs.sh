397de860a6c0fa3869771cf8f2977f224fd78586
ethanewer <eewer@wisc.edu>
2024-08-08 18:38:56 +0000

    uncomment

--- run_configs.sh
+++ run_configs.sh
@@ -6,10 +6,8 @@ for FILE in configs/*; do
     if [ -f "$FILE" ]; then
         if [[ "$FILE" == *.json ]]; then
             python generate_addition_data.py "$FILE"
-            echo "$FILE"
-            wc -l data/addition/train_plain_addition.txt
-            # python train.py "$FILE"
-            # python evaluate.py "$FILE"
+            python train.py "$FILE"
+            python evaluate.py "$FILE"
         fi
     fi
 done
