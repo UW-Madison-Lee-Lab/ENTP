e135edc846ddc17fd398c88a0bb8bf7917f449f1
Ethan <ewer@wisc.edu>
2024-08-12 00:40:25 -0500

    progress report and updated transformer docstrings

--- nano_transformer/transformer.py
+++ nano_transformer/transformer.py
@@ -24,7 +24,6 @@ class Transformer(nn.Module):
         if compile_blocks:
             for block in self.h:
                 block.compile()
-            
 
     @staticmethod
     def __init_weights(module: nn.Module) -> None:
@@ -36,7 +35,7 @@ class Transformer(nn.Module):
             nn.init.normal_(module.weight, mean=0.0, std=0.02)
 
     def __decoder_forward(self, x: Tensor, is_causal=True) -> Tensor:
-        """Helper method for forward. Causal forward is O(N^2.)"""
+        """Helper method for forward. Decoder forward is O(N^2)."""
         for block in self.h:
             x = block(x, is_causal)
         return self.ln_f(x)
@@ -47,14 +46,14 @@ class Transformer(nn.Module):
         forward_idxs: Optional[Sequence[int]],
         is_causal=False,
     ) -> Tensor:
-        """Helper method for forward. Noncausal forward is O(N^3.)"""
+        """Helper method for forward. Encoder forward is O(N^3)."""
         y = torch.zeros_like(x)
         for t in range(x.shape[1]):
             if forward_idxs is None or t in forward_idxs:
-                x_t = x[:, : t + 1, :]
+                x_t = x[:, : t + 1]
                 for block in self.h:  # type: ignore
                     x_t = block(x_t, is_causal)
-                y[:, t, :] = self.ln_f(x_t[:, t, :])
+                y[:, t] = self.ln_f(x_t[:, t])
         return y
 
     def forward(
@@ -104,7 +103,7 @@ class TransformerLMHead(nn.Module):
                 input_ids = input_ids[:, -self.n_positions :]
 
             logits = self(input_ids, decoder, forward_idxs=(input_ids.shape[1] - 1,))
-            next_id = torch.argmax(logits[:, -1:, :], dim=2)
+            next_id = torch.argmax(logits[:, -1:], dim=2)
             input_ids = torch.cat((input_ids, next_id), dim=1)
 
         return input_ids
