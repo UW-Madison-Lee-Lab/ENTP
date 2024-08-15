68cee445aadb7b7d0a7be828d6f99f451a5b3ff8
Ethan <ewer@wisc.edu>
2024-08-15 00:36:59 -0500

    more results

--- generate_configs.py
+++ generate_configs.py
@@ -6,7 +6,6 @@ BASE_CONFIG: dict[str, bool | int | float | str] = {
     "n_test": 70000,
     "n_val": 10000,
     "max_evals_without_improving": 5,
-    "max_loss_for_early_stopping": 1e9,
     "batch_size": 32,
 }
 
@@ -19,10 +18,10 @@ def n_train_str(n_train: int) -> str:
 
 
 if __name__ == "__main__":
-    for n_train in [2500, 3750]:
+    for n_train in [3750]:
         for decoder in [False]:
-            for task in ["plain_addition"]:
-                for seed in range(5):
+            for task in ["reversed_addition"]:
+                for seed in range(3, 5):
                     name = f"{n_train_str(n_train)}_{task}_{'decoder' if decoder else 'encoder'}_{seed}"
                     config = copy.deepcopy(BASE_CONFIG)
                     config["n_train"] = n_train
@@ -32,7 +31,7 @@ if __name__ == "__main__":
                     config["name"] = name
                     config["results_dir"] = f"results/{n_train_str(n_train)}"
 
-                    if n_train == 5000:
+                    if n_train <= 5000:
                         config["max_loss_for_early_stopping"] = 1e9
 
                     config_path = f"configs/{name}.json"
