c2ddb220a61cd193c1a7c52f630c4fab60c58a76
Ethan <ewer@wisc.edu>
2024-08-16 11:07:10 -0500

    update generate configs

--- generate_configs.py
+++ generate_configs.py
@@ -29,27 +29,7 @@ def n_train_str(n_train: int) -> str:
 
 
 if __name__ == "__main__":
-    for n_train in [3750]:
-        for decoder in [False]:
-            for task in ["reversed_addition"]:
-                for seed in range(3, 5):
-                    name = f"{n_train_str(n_train)}_{task}_{'decoder' if decoder else 'encoder'}_{seed}"
-                    config = copy.deepcopy(BASE_CONFIG)
-                    config["n_train"] = n_train
-                    config["decoder"] = decoder
-                    config["task"] = task
-                    config["seed"] = seed
-                    config["name"] = name
-                    config["results_dir"] = f"results/{n_train_str(n_train)}"
-
-                    if n_train <= 5000:
-                        config["max_loss_for_early_stopping"] = 1e9
-
-                    config_path = f"configs/{name}.json"
-                    with open(config_path, "w") as f:
-                        json.dump(config, f)
-
-    for n_train in [2500, 5000, 10000, 20000]:
+    for n_train in [2500, 5000]:
         for decoder in [True, False]:
             for task in ["plain_addition", "reversed_addition"]:
                 for seed in range(5):
