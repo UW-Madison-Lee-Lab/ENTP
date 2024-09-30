088cd3d2496c6dce385be3f3f09365ff09637f25
Ethan <ewer@wisc.edu>
2024-09-29 21:48:45 -0500

    new seeds

--- generate_configs.py
+++ generate_configs.py
@@ -88,7 +88,7 @@ def n_train_str(n_train: int) -> str:
 if __name__ == "__main__":
     for size in [EXTRA_SMALL_DEEP]:
         for decoder in [True, False]:
-            for seed in range(1):
+            for seed in range(1, 3):
                 name = BASE_CONFIG["task"]
                 name += f"_{size['size_name']}"
                 name += "_decoder" if decoder else "_encoder"
